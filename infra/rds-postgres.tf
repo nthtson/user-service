e84bbf8bc46d40d09962743d0de2bc4b8b20d371
nthtson <nguyentranhothanhson@gmail.com>
2025-04-25 17:05:59 +0700

    fix Helm migrations

--- infra/rds-postgres.tf
+++ infra/rds-postgres.tf
@@ -33,6 +33,7 @@ resource "aws_db_instance" "postgres" {
   username               = var.db_user
   password               = var.db_password
 
+  publicly_accessible     = true
   vpc_security_group_ids = [aws_security_group.rds.id]
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
 
